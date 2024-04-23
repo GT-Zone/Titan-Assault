39af5f4a577dde70fcaccee2a95176aeada69fb1
Azlan <azlanehtesham02@gmail.com>
2024-04-24 01:13:20 +0530

    creating file stucture and writing starter code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center w-full">
+        <h1 className="text-2xl">Titan Assault</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
